43d6e47c5f76e7b691486c47e6832f168fcf782a
Matheus-C-Machado <158229786+Matheus-C-Machado@users.noreply.github.com>
2024-10-14 19:56:10 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Cabecalho from "./components/Cabecalho";
+import Calculadora from "./components/Calculadora";
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <Cabecalho />
+        <Calculadora />
+
+      </div>
+    </>
+  )
+}
+
+export default App;
